067af45b792551207199c80a295e9e8394b113b7
Maurício R Duarte <mauriciotem@gmail.com>
2017-05-26 18:18:58 -0300

    Store do Redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import styles from './App.scss';
 
-const App = () => (
-  <div>
-    <h1>Meu Primeiro App React</h1>
-  </div>
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+//import ReduxThunk from 'redux-thunk';
+import reducers from '../reducers';
+
+import TarefasApp from './todo/components/TarefasApp';
+
+const store = createStore(
+  reducers
+);
+
+const App = (props) => (
+  <Provider store={store}>
+    <TarefasApp />
+  </Provider>
 );
 
 export default App;
