92270e5376fc29b16285a99a0404e69d46431327
Maurício R Duarte <mauriciord@me.com>
2017-05-27 17:12:23 -0300

    Organização dos módulos

---
+++ src/actions/index.js
@@ -0,0 +1,2 @@
+export * from '../components/todo/actions/TarefasActions';
+export * from '../components/filtros/actions/FiltroActions';
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import TarefasReducer from '../components/todo/reducers/TarefasReducer';
-import FiltroReducer from '../components/todo/reducers/FiltroReducer';
+import FiltroReducer from '../components/filtros/reducers/FiltroReducer';
 
 export default combineReducers({
   tarefas: TarefasReducer,
