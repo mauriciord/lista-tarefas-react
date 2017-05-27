37eb3856350725bf5e4e71be7b99fc20c7e05a10
Maurício R Duarte <mauriciord@me.com>
2017-05-27 17:11:06 -0300

    LocalStorage save e load

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './App.scss';
+import { loadState, saveState } from '../localStorage';
+import throttle from 'lodash/throttle';
 
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
@@ -8,9 +10,18 @@ import reducers from '../reducers';
 
 import TarefasApp from './todo/components/TarefasApp';
 
+const persistedState = loadState();
 const store = createStore(
-  reducers
+  reducers,
+  persistedState
 );
+
+// aqui eu salvo na localStorage
+store.subscribe(throttle(() => {
+  saveState({
+    tarefas: store.getState().tarefas
+  });
+}, 1000));
 
 const App = (props) => (
   <Provider store={store}>
