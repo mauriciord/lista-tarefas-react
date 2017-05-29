1b75774052f982c28dcd01ce2a0f530ed45b3e68
Maurício R Duarte <mauriciord@me.com>
2017-05-28 22:13:03 -0300

    nas actions adicionei o ordenador

--- src/components/filtros/actions/FiltroActions.js
+++ src/components/filtros/actions/FiltroActions.js
@@ -1,10 +1,18 @@
 import {
-  SET_VISIBILIDADE_FILTRO
+  SET_VISIBILIDADE_FILTRO,
+  SET_VISIBILIDADE_ORDEM
 } from './types';
 
 export const filtrar = (filtro) => {
   return {
     type: SET_VISIBILIDADE_FILTRO,
     payload: filtro
+  }
+};
+
+export const ordenar = (ordenador) => {
+  return {
+    type: SET_VISIBILIDADE_ORDEM,
+    payload: ordenador
   }
 };
